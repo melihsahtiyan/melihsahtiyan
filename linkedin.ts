1c0d19502ef3ffaad6cb5a3e58fe6e198d55479c
melihsahtiyan <77502445+melihsahtiyan@users.noreply.github.com>
2021-04-20 23:25:26 +0300

    Create linkedin.ts

---
+++ linkedin.ts
@@ -0,0 +1,5 @@
+<div class="LI-profile-badge"  data-version="v1" data-size="medium" data-locale="tr_TR" data-type="horizontal" data-theme="dark" data-vanity="melihsahtiyan">
+  <a class="LI-simple-link" href='https://tr.linkedin.com/in/melihsahtiyan?trk=profile-badge'>
+    Melih Sahtiyan
+  </a>
+</div>
